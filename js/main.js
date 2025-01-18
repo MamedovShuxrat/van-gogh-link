20b7e48a3b56d47c9e376aa5d9b1a9b6238e1dd0
Шухрат <lider_20_10@mail.ru>
2025-01-18 12:28:54 +0300

    JS logic has been added, mobile phone hovers have been removed, minor bugs have been fixed

--- js/main.js
+++ js/main.js
@@ -8,12 +8,39 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     document.addEventListener('click', (event) => {
-        const isClickInsideNav = nav.contains(event.target);
-        const isClickInsideBurger = burger.contains(event.target);
+        const isNavigationClick = nav.contains(event.target);
+        const isBurgerClick = burger.contains(event.target);
 
-        if (!isClickInsideNav && !isClickInsideBurger && nav.classList.contains('open')) {
+        if (!isNavigationClick && !isBurgerClick && nav.classList.contains('open')) {
             burger.classList.remove('open');
             nav.classList.remove('open');
         }
     });
+
+    const searchInput = document.querySelector('.search-cart__input')
+    const cards = document.querySelectorAll('.card__item')
+    const resultTitle = document.querySelector('.result__title')
+
+    function filterCards() {
+        const searchQuery = searchInput.value.toLowerCase()
+        let found = false
+
+        cards.forEach(card => {
+            const cardName = card.querySelector('.card__name').textContent.toLocaleLowerCase()
+
+            if (cardName.includes(searchQuery)) {
+                card.classList.remove('hide')
+                found = true
+            } else {
+                card.classList.add('hide')
+            }
+        })
+        if (found) {
+            resultTitle.textContent = 'Результаты поиска'
+        } else {
+            resultTitle.textContent = 'Ничего не найдено'
+
+        }
+    }
+    searchInput.addEventListener('input', filterCards)
 })
